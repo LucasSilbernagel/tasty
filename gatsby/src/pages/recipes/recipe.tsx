6fa65e5de07f9b0c2033abb48c5624ce8a5ee87c
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-10-27 22:07:39 -0400

    updated recipe page

--- gatsby/src/pages/recipes/recipe.tsx
+++ gatsby/src/pages/recipes/recipe.tsx
@@ -1,46 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { HeadFC, PageProps } from 'gatsby'
-
-interface IDirectionData {
-  directions: string
-}
-
-interface IIngredientData {
-  ingredients: string
-}
-
-interface IIntroText {
-  introText: string
-}
-
-interface IPhoto {
-  url: string
-  alternativeText: string
-}
-
-interface IAuthor {
-  bio: string
-  jobTitle: string
-  name: string
-  photo: IPhoto
-}
-
-interface RecipeContext {
-  author: IAuthor
-  id: string
-  directions: IDirectionData
-  ingredients: IIngredientData
-  introText: IIntroText
-  largePhoto: IPhoto
-  smallPhoto: IPhoto
-  name: string
-  prepTime: string
-  publishedAt: string
-  recipeSlug: string
-  tagline: string
-  totalTime: string
-  yields: string
-}
+import { HeadFC } from 'gatsby'
 
 export const Head: HeadFC = ({ pageContext }: any) => {
   const { name } = pageContext
@@ -48,15 +7,10 @@ export const Head: HeadFC = ({ pageContext }: any) => {
 }
 
 const RecipePage = ({ pageContext }: any) => {
-  // eslint-disable-next-line react/prop-types
-  // const { name } = data.strapiRecipe
-  const { name, recipeSlug } = pageContext
-  // const data = useStaticQuery(query)
-  // console.log(query)
-  // console.log(window.location.pathname.split('/')[2])
+  const { name } = pageContext
   return (
     <main>
-      <h1>{name}</h1>
+      <h2>{name}</h2>
     </main>
   )
 }
