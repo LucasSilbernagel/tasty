a165472ad03d892e09431e784d075bae231a33fc
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-11-03 22:36:39 -0400

    styled the 404 page

--- gatsby/src/pages/404.tsx
+++ gatsby/src/pages/404.tsx
@@ -1,16 +1,26 @@
 import { Link, HeadFC } from 'gatsby'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <title>Tasty | Not found</title>
 
 const NotFoundPage = () => {
   return (
-    <main>
-      <h1>Page not found</h1>
-      <p>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <>
+      <Header />
+      <main className="min-h-[72vh] py-6 px-2 mx-auto max-w-screen-lg text-center">
+        <h1 className="font-black text-4xl mt-20 mb-8">
+          Oops! We don&apos;t have the page you&apos;re looking for.
+        </h1>
+        <p className="text-xl">
+          <Link className="underline underline-offset-4" to="/">
+            Return to the homepage
+          </Link>
+          .
+        </p>
+      </main>
+      <Footer />
+    </>
   )
 }
 
