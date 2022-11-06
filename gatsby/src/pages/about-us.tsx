39f60712db09e58b177a514a2ddcfe892317c15a
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-11-06 17:52:24 -0500

    added header and footer to about page

--- gatsby/src/pages/about-us.tsx
+++ gatsby/src/pages/about-us.tsx
@@ -1,12 +1,18 @@
 import { HeadFC } from 'gatsby'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
 
 export const Head: HeadFC = () => <title>Tasty | About Us</title>
 
 const AboutUsPage = () => {
   return (
-    <main>
-      <h1>About us page</h1>
-    </main>
+    <>
+      <Header />
+      <main>
+        <h1>About Us</h1>
+      </main>
+      <Footer />
+    </>
   )
 }
 
