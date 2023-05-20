879908e6ce0ddf5bad545e32d349038656fb44d6
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-05-19 22:14:47 -0400

    updated database configuration

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,16 +1,39 @@
 import path from 'path'
+import { parse } from 'pg-connection-string'
+
+const developmentConfig = {
+  client: 'sqlite',
+  connection: {
+    filename: path.join(__dirname, '..', '..', '.tmp/data.db'),
+  },
+  useNullAsDefault: true,
+}
+
+const productionConfig = {
+  client: 'postgres',
+  connection: {
+    host: process.env.PGHOST || 'localhost',
+    port: process.env.PGPORT || 5432,
+    database: process.env.PGDATABASE,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    ssl: { rejectUnauthorized: false },
+    ...parse(process.env.DATABASE_URL || ''),
+  },
+  pool: {
+    min: 0,
+    max: 10,
+    acquireTimeoutMillis: 60000,
+    idleTimeoutMillis: 30000,
+    createTimeoutMillis: 30000,
+    reapIntervalMillis: 1000,
+    createRetryIntervalMillis: 100,
+  },
+}
 
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(
-        __dirname,
-        '..',
-        '..',
-        env('DATABASE_FILENAME', '.tmp/data.db')
-      ),
-    },
-    useNullAsDefault: true,
-  },
+    development: developmentConfig,
+    production: productionConfig,
+  }[env('NODE_ENV', 'development')],
 })
