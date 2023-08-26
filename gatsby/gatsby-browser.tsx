2a5484b2448c37531d115dc43234c8b35a614fa6
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-08-26 17:35:51 -0400

    setting up Gatsby SSR

---
+++ gatsby/gatsby-browser.tsx
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react'
+import './src/styles/global.css'
+
+export const wrapRootElement = ({ element }) => <div>{element}</div>
